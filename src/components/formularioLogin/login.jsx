c8acb30f035bfe9774fac3d36972534495e10432
Jhack Costa <jhacktibia@gmail.com>
2022-04-26 23:53:04 -0300

    form de login reparado e acrescentado

--- src/components/formularioLogin/login.jsx
+++ src/components/formularioLogin/login.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import {Button} from "../styled-components/button/button.jsx";
 import {Input} from "../styled-components/input/input.jsx";
 import {Label} from "../styled-components/label/label.jsx"
+
 import styles from "./login.module.css";
 
 function Login() {
@@ -22,42 +23,47 @@ function Login() {
 		<div className={styles.principal}>
 
 			<div className={styles.propaganda}>
-				<img className={styles.imagem} src="https://i.pinimg.com/originals/ae/bb/95/aebb950866dfc6c867f80a80cc27f370.jpg" alt="Casal Saúdavel"/>		
-			</div>
-			
-			<form className={styles.form} onSubmit={(e) => login(e)}>
-				<div className={styles.home}>
-					<Label >
-						<div className={styles.corpo}>
-							<p>Digite seu e-mail: </p>
-							<Input type="email" 
-							className={styles.input} 
-							placeholder = "E-mail"
-							onChange={handleOnChange}
+					 {/* <img className={styles.imagem} src="https://i.pinimg.com/originals/ae/bb/95/aebb950866dfc6c867f80a80cc27f370.jpg" alt="Casal Saúdavel"/>	 */}
+			</div> 
+
+				<div className={styles.login}>
+
+					<h1>Fazer Login!</h1>
+
+					<form className={styles.form} onSubmit={(e) => login(e)}>
 							
-							/>
-							<p>Digite sua senha: </p>
-							<Input type="password"
+						<Label className={[styles.label, styles.email ].join(' ')}>
+							Digite seu E-mail:																				
+							<Input type="email" 														
+							placeholder = "E-mail"
 							className={styles.input}
-							placeholder = "Password"
 							onChange={handleOnChange}
-							/>
-
-
-
+							required
+							/>														
+						</Label>
+							
+						<Label className={[styles.label, styles.senha ].join(' ')}>
+								Digite sua senha: 
+							
+								<Input type="password"
+								placeholder = "Password"
+								className={styles.input}
+								onChange={handleOnChange}
+								required
+								/>							
+						<div className={styles.cadastro}>
+						<a className={styles.tagA} href="https://www.w3schools.com">Novo cadastro...</a>
+						<a className={styles.tagA} href="https://www.w3schools.com">Recuperar Cadastro...</a>
 						</div>
+							
+						</Label>
+						
 						<div className={styles.rodaPe}>
-							<Button type="submit" className={styles.botao}>Entrar</Button>
-							<Button type="submit" className={styles.botao}>Esqueci Senha</Button>
-							<Button type="submit" className={styles.botao}>Criar conta</Button>
+							<Button type="submit" className={styles.botao}>Entrar</Button>												
 						</div>			
-					</Label>
+					</form>
 				</div>
 
-			</form>
-			<div className={styles.propaganda}>
-				<img className={styles.imagem} src="https://i.pinimg.com/originals/ae/bb/95/aebb950866dfc6c867f80a80cc27f370.jpg" alt="Casal Saúdavel"/>		
-			</div>
 
 		</div>
     );
--- src/scripts/services/api.js
+++ src/scripts/services/api.js
@@ -4,12 +4,9 @@ const api = axios.create({
   baseURL: "https://localhost:3000",
 });
 
-
 export const cadastro = axios.create({
     baseURL: "https://api-loja-t11.herokuapp.com"
 })
-
-
 
 //! used for authentication 
 // api.defaults.headers.authorization = `Bearer ${token}`;
