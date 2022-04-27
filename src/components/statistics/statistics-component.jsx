1e9ba0f567c99ffff2141b4f2e202b00421420f2
Jhack Costa <jhacktibia@gmail.com>
2022-04-27 20:46:32 -0300

    teste commit

--- src/components/statistics/statistics-component.jsx
+++ src/components/statistics/statistics-component.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
-
-
-
 export const StatisticsComponent = styled.div`
     position: relative; 
     background-color: rgb(240,240,240);
