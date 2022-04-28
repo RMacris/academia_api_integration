1c686c586c873efd5d409d6842ec96d42a456725
Rafael Soares <rafa.macris@gmail.com>
2022-04-27 21:42:44 -0300

    fixing UserLogin erros

--- src/scripts/services/userLogin.js
+++ src/scripts/services/userLogin.js
@@ -1,9 +1,9 @@
 
 export class UserLogin { 
     constructor(user = {id: 0, nome: '', email: '', senha: ''}) { 
-        this.$id = user.id
-        this.$nome = user.nome
-        this.$email = user.email
-        this.$senha = user.senha
+        this.id = user.id
+        this.nome = user.nome
+        this.email = user.email
+        this.senha = user.senha
     }
 }   
